29f2eb4834a3a5947a575e107a78dd8026e224f7
“박현진” <imhjinnnnn@gmail.com>
2025-05-04 15:08:27 +0900

    fix: google auth error

--- index.js
+++ index.js
@@ -160,15 +160,14 @@ async function logErrorToSheet(auth, errorMessage) {
 (async () => {
   try {
     let credentials;
+
     if (process.env.GOOGLE_SERVICE_KEY) {
+      // 깃헙 시크릿
       credentials = JSON.parse(process.env.GOOGLE_SERVICE_KEY);
-    } else {
-      // 로컬 환경에서는 'credentials.json' 파일에서 읽기
-      credentials = CREDENTIALS_PATH;
     }
 
     const auth = new google.auth.GoogleAuth({
-      keyFile: credentials,
+      ...(credentials ? { credentials } : { keyFile: CREDENTIALS_PATH }),
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
 
